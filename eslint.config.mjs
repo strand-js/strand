caa6efbd6904394baa2e782eb2b7eefe61e64a24
Stephen <sbullocks@gmail.com>
2026-06-19 16:16:45 -0400

    chore: add root workspace config (pnpm, Turborepo, TS, ESLint, Prettier, Changesets)

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import js from '@eslint/js'
+import tsPlugin from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['packages/*/src/**/*.ts', 'packages/*/src/**/*.tsx'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+    },
+    plugins: { '@typescript-eslint': tsPlugin },
+    rules: {
+      ...tsPlugin.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    ignores: ['**/dist/**', '**/node_modules/**', '**/*.config.*'],
+  },
+]
